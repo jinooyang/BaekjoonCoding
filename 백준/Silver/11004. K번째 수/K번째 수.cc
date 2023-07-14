87759cbe24d1061aae31d7d2e35a4786de71f66d
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-07-14 13:23:02 +0900

    [Silver V] Title: K번째 수, Time: 1064 ms, Memory: 51300 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+	int n, k;
+	cin >> n >> k;
+	vector<int> v;
+	for (int i = 0; i < n; i++) {
+		int temp; cin >> temp;
+		v.push_back(temp);
+
+	}
+	sort(v.begin(), v.end());
+	cout << v[k-1];
+}
