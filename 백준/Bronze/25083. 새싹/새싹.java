24d5a11cb4874d44abd06e9e495b3c7bdb1d83ab
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-08-01 21:46:33 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Main {
+	static int n = 0;
+	static int cnt = 0;
+
+	public static void main(String[] args) throws IOException {
+		String s = "         ,r'\"7\n" + 
+				"r`-_   ,'  ,/\n" + 
+				" \\. \". L_r'\n" + 
+				"   `~\\/\n" + 
+				"      |\n" + 
+				"      |";
+		System.out.print(s);
+	}
+}
