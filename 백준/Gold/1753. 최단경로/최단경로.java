f29c928921554063843664f30c6f63d43c2517bc
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-08-01 17:41:22 +0900

    [Gold IV] Title: 최단경로, Time: 1168 ms, Memory: 110928 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Node implements Comparable<Node> {
+        int vertex;
+        int dist;
+
+        public Node(int vertex, int dist) {
+            this.dist = dist;
+            this.vertex = vertex;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.dist, o.dist);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+
+        List<List<Node>> map = new ArrayList<>();
+        for (int i = 0; i < V + 1; i++) {
+            map.add(new ArrayList<>());
+        }
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            map.get(u).add(new Node(v, w));
+        }
+
+        int[] distance = new int[V + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+        distance[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            int now = node.vertex;
+            int dist = node.dist;
+
+            if (dist > distance[now]) {
+                continue;
+            }
+
+            for (Node nextNode : map.get(now)) {
+                int newDistance = distance[now] + nextNode.dist;
+                if (newDistance < distance[nextNode.vertex]) {
+                    distance[nextNode.vertex] = newDistance;
+                    pq.add(new Node(nextNode.vertex, newDistance));
+                }
+            }
+        }
+
+        for (int i = 1; i <= V; i++) {
+            if (distance[i] == Integer.MAX_VALUE) {
+                System.out.println("INF");
+            } else {
+                System.out.println(distance[i]);
+            }
+        }
+    }
+}
