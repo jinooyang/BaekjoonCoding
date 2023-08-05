bd9decafc9be557669aa9c1f4a0fc617f651a007
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-08-05 21:13:54 +0900

    [Silver III] Title: 계단 오르기, Time: 80 ms, Memory: 11592 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int ary[] = new int[n + 3];
+		for (int i = 3; i < n + 3; i++) {
+			st = new StringTokenizer(br.readLine());
+			ary[i] = Integer.parseInt(st.nextToken());
+		}
+		int dp[][] = new int[n + 3][2];
+		for (int i = 3; i < n + 3; i++) {
+			int a = 0;// dp i-3 + aryi-2 + aryi
+			int b = 0;// dp i-3 + aryi-1 + aryi
+			int c = 0;// dp i-2 + aryi
+			int cnt = 0;
+			if (!(dp[i - 3][1] == 2)) {
+				a = dp[i - 3][0] + ary[i - 2] + ary[i];
+			}
+			b = dp[i - 3][0] + ary[i - 1] + ary[i];
+			c = dp[i - 2][0] + ary[i];
+			if (a >= b) {
+				dp[i][0] = a;
+				dp[i][1] = 1;
+			} else {
+				dp[i][0] = b;
+				dp[i][1] = 2;
+			}
+			if (c > dp[i][0]) {
+				dp[i][0] = c;
+				dp[i][1] = 1;
+			}
+
+		}
+		System.out.println(dp[n + 2][0]);
+	}
+}
