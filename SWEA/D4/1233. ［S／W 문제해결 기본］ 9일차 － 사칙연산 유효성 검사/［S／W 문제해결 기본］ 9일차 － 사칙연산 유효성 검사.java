f190eda2e7923598f55202c82ce1b54e57a99fd5
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-08-08 17:25:15 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 119 ms, Memory: 19,372 KB -BaekjoonHub

--- SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -1,19 +1,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 class Solution {
 
 	public static void main(String args[]) throws Exception {
-		Set<Character> set = new HashSet<>();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st = null;
 		int T = 10;
 		for (int test_case = 1; test_case <= T; test_case++) {
-
 			st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			int answer = 1;
@@ -35,23 +30,18 @@ class Solution {
 
 				if (value == '*' || value == '/' || value == '+' || value == '-') {
 					// 연산자 일 경우
-					if (leftNode == 0 || rightNode == 0) {// 자식이 둘 있어야한다
-						//System.out.println("test_case : " + test_case + "operator has no child : " + i);
+					if (leftNode == 0 || rightNode == 0) // 자식이 둘 있어야한다
 						answer = 0;
-					}
 				} else {
 					// 숫자일 경우
-					if (leftNode != 0 || rightNode != 0) {// 자식이 있으면 안된다
-						//System.out.println("test_case : " + test_case + "number has child : " + i);
-						//System.out.println("value is : " + value);
+					if (leftNode != 0 || rightNode != 0) // 자식이 있으면 안된다
 						answer = 0;
-					}
 				}
 			}
 
 			System.out.println("#" + test_case + " " + answer);
 		}
-		//System.out.println("fin");
+
 	}
 
 }
