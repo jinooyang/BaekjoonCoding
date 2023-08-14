5ff15e980f9b1fc001648fef17e1f675cb6b7482
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-08-14 16:21:39 +0900

    [Silver I] Title: Z, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -3,14 +3,11 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int r = Integer.parseInt(st.nextToken());
@@ -22,45 +19,32 @@ public class Main {
 				break;
 			}
 
-			int sq = getBiggestSq(r, c);// 가장 큰 2^n의 n
+			int sq = getBiggestSq(r, c); // 가장 큰 2^n의 n
 
-			int maxsq = getPow(sq);// 2^n
+			int maxsq = 1 << sq; // 2^n using bitwise left shift
 			int mul = 0;
 			if (r >= maxsq) {
 				mul += 2;
 				r -= maxsq;
-
 			}
 			if (c >= maxsq) {
 				mul += 1;
 				c -= maxsq;
 			}
 			answer += maxsq * maxsq * mul;
-
 		}
 		System.out.println(answer);
-
-	}
-
-	private static int getPow(int sq) {
-		int mul = 1;
-		for (int i = 0; i < sq; i++) {
-			mul *= 2;
-		}
-		return mul;
 	}
 
 	private static int getBiggestSq(int r, int c) {
 		int answer = 0;
 		while (true) {
-			if (Math.pow(2, answer + 1) > r && Math.pow(2, answer + 1) > c)
+			int k = 1 << (answer + 1);
+			if (k > r && k > c)
 				break;
 			else
 				answer++;
-
 		}
-
 		return answer;
-
 	}
 }
