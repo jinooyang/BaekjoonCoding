ae3390a37aba4df31e75f6c6744a8bb929462fb4
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-08-14 17:32:45 +0900

    [Silver I] Title: Z, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -9,19 +9,20 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken()) - 1;
 		int r = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
 
 		int answer = 0;
+
 		while (true) {
-			if (r == 0 && c == 0) {
+			if (r == 0 && c == 0)
 				break;
+			if (r < n && c < n) {
+				n--;
+				continue;
 			}
-
-			int sq = getBiggestSq(r, c); // 가장 큰 2^n의 n
-
-			int maxsq = 1 << sq; // 2^n using bitwise left shift
+			int maxsq = 1 << n; // 2^n using bitwise left shift
 			int mul = 0;
 			if (r >= maxsq) {
 				mul += 2;
@@ -31,20 +32,11 @@ public class Main {
 				mul += 1;
 				c -= maxsq;
 			}
-			answer += maxsq * maxsq * mul;
+			answer += (1 << n << n) * mul;
+			n -= 1;
+
 		}
 		System.out.println(answer);
 	}
 
-	private static int getBiggestSq(int r, int c) {
-		int answer = 0;
-		while (true) {
-			int k = 1 << (answer + 1);
-			if (k > r && k > c)
-				break;
-			else
-				answer++;
-		}
-		return answer;
-	}
 }
