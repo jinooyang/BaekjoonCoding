14e4f8dcc63c9a4b28672ee09f0154bebcc1f0cd
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-08-17 22:51:35 +0900

    [Gold I] Title: 구간 합 구하기, Time: 940 ms, Memory: 143108 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -10,58 +10,64 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		ary = new long[N];
-		seg = new long[4 * N + 1];
-		for (int i = 0; i < N; i++) {
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		ary = new long[n];
+		seg = new long[4 * n + 1];
+
+		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			ary[i] = Long.parseLong(st.nextToken());
 		}
-		segTree(0, N - 1, 1);
-		for (int i = 0; i < M + K; i++) {
+		init(0, n - 1, 1);
+		// System.out.println(n + Arrays.toString(seg));
+		for (int i = 0; i < m + k; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			long c = Long.parseLong(st.nextToken());
-
-			if (a == 1) {
+			if (a == 1) {// update
 				long diff = c - ary[b - 1];
-				update(0, N - 1, 1, b - 1, diff);
 				ary[b - 1] = c;
+				update(0, n - 1, 1, b - 1, diff);
+				// System.out.println(Arrays.toString(seg));
+
 			}
-			if (a == 2) {
-				long sum = getSum(0, N - 1, 1, b - 1, c - 1);
+			if (a == 2) {// sum
+				long sum = sum(0, n - 1, 1, b - 1, c - 1);
 				System.out.println(sum);
 			}
 		}
 	}
 
-	private static long getSum(int start, int end, int node, int left, long right) {
-		if (left > end || right < start)
+	private static long sum(int s, int e, int n, int l, long r) {
+		if (r < s || e < l) {
 			return 0;
-		if (left <= start && right >= end)
-			return seg[node];
-		int mid = (start + end) / 2;
-		return getSum(start, mid, node * 2, left, right) + getSum(mid + 1, end, node * 2 + 1, left, right);
+		}
+		if (l <= s && e <= r) {
+			return seg[n];
+		}
+		int mid = (s + e) / 2;
+		return sum(s, mid, n * 2, l, r) + sum(mid + 1, e, n * 2 + 1, l, r);
 	}
 
-	private static void update(int start, int end, int node, int changeidx, long diff) {
-		if (changeidx < start || changeidx > end)
+	private static void update(int s, int e, int n, int c, long d) {
+		if (c < s || c > e) {
 			return;
-		seg[node] += diff;
-		if (start == end)
+		}
+		seg[n] += d;
+		if (s == e)
 			return;
-		int mid = (start + end) / 2;
-		update(start, mid, node * 2, changeidx, diff);
-		update(mid + 1, end, node * 2 + 1, changeidx, diff);
+		int mid = (s + e) / 2;
+		update(s, mid, n * 2, c, d);
+		update(mid + 1, e, n * 2 + 1, c, d);
 	}
 
-	private static long segTree(int start, int end, int node) {
-		if (start == end)
-			return seg[node] = ary[start];
-		int mid = (start + end) / 2;
-		return seg[node] = segTree(start, mid, node * 2) + segTree(mid + 1, end, 2 * node + 1);
+	private static long init(int s, int e, int n) {
+		if (s == e)
+			return seg[n] = ary[s];
+		int mid = (s + e) / 2;
+		return seg[n] = init(s, mid, 2 * n) + init(mid + 1, e, 2 * n + 1);
 	}
 }
