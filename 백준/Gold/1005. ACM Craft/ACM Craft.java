9c0ff76658b78702390cb4e9189a3b58a4b5c511
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-08-21 23:28:43 +0900

    [Gold III] Title: ACM Craft, Time: 1436 ms, Memory: 250572 KB -BaekjoonHub

---
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		for (int tc = 0; tc < T; tc++) {
+			st = new StringTokenizer(br.readLine());
+
+			int n = Integer.parseInt(st.nextToken());// 건물의 개수 N
+			int k = Integer.parseInt(st.nextToken());// 건물간의 건설순서 규칙의 총 개수 K
+			st = new StringTokenizer(br.readLine());
+			int time[] = new int[n + 1];// 건물 짓는 시간
+			int answer[] = new int[n + 1];
+			for (int i = 1; i <= n; i++) {
+				time[i] = Integer.parseInt(st.nextToken());
+				answer[i] = time[i];
+			}
+			int building[] = new int[n + 1];// depth
+
+			// Arrays.fill(answer, 0);
+			List<List<Integer>> list = new ArrayList<>();
+
+			for (int i = 0; i < n + 1; i++) {
+				list.add(new ArrayList<>());
+			}
+			for (int i = 0; i < k; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				building[b]++;
+				list.get(a).add(b);
+			}
+			st = new StringTokenizer(br.readLine());
+			int w = Integer.parseInt(st.nextToken());
+			Queue<Integer> q = new ArrayDeque<>();
+			Queue<Integer> qcnt = new ArrayDeque<>();
+
+			for (int i = 1; i <= n; i++) {
+				if (building[i] == 0) {
+					q.add(i);
+					qcnt.add(time[i]);
+				}
+			}
+			while (!q.isEmpty()) {
+				int now = q.poll();
+				// System.out.println(now);
+				int cnt = qcnt.poll();
+				// System.out.println(now + " " + cnt);
+				// if (cnt > answer[now])
+				// answer[now] = cnt;
+				for (int i = 0; i < list.get(now).size(); i++) {
+					int next = list.get(now).get(i);
+					int newTime = answer[now] + time[next];
+					building[next]--;
+					if (answer[next] < newTime)
+						answer[next] = newTime;
+					if (building[next] == 0) {
+						q.add(next);
+						qcnt.add(newTime);
+					}
+				}
+			}
+			System.out.println(answer[w]);
+			// System.out.println("----");
+		}
+	}
+}
