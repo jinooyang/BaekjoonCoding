5d6f61db4d28e297d40b833ba7c754cb57ed3269
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-08-22 19:50:02 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 80 ms, Memory: 11632 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int ary[] = new int[n + 1];
+		for (int i = 0; i < n + 1; i++) {
+			ary[i] = i;
+		}
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			swap(ary, a, b);
+
+		}
+		for (int i = 1; i <= n; i++) {
+			System.out.print(ary[i] + " ");
+		}
+		System.out.println();
+
+	}
+
+	private static void swap(int[] ary, int a, int b) {
+		while (a < b) {
+			int temp = ary[a];
+			ary[a] = ary[b];
+			ary[b] = temp;
+			a++;
+			b--;
+		}
+	}
+}
