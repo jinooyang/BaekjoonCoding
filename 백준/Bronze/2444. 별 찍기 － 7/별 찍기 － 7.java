63f26aef78266731f698af112764c18d07c8f2c1
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-08-24 11:40:09 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 84 ms, Memory: 11736 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -5,35 +5,34 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		for (int i = 0; i < n; i++) {
-			int max = 2 * n - 1;
-			for (int j = 0; j < max / 2 - i; j++) {
-				System.out.print(" ");
-			}
-			for (int j = 0; j < 2 * i + 1; j++) {
-				System.out.print("*");
-			}
-//			for (int j = 0; j < max / 2; j++) {
-//				System.out.print(" ");
-//			}
-			System.out.println();
-		}
-		for (int i = n - 2; i >= 0; i--) {
-			int max = 2 * n - 1;
-			for (int j = 0; j < max / 2 - i; j++) {
-				System.out.print(" ");
-			}
-			for (int j = 0; j < 2 * i + 1; j++) {
-				System.out.print("*");
-			}
-//			for (int j = 0; j < max / 2; j++) {
-//				System.out.print(" ");
-//			}
-			System.out.println();
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        StringBuilder output = new StringBuilder(); // StringBuilder 생성
+
+        for (int i = 0; i < n; i++) {
+            int max = 2 * n - 1;
+            for (int j = 0; j < max / 2 - i; j++) {
+                output.append(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                output.append("*");
+            }
+            output.append("\n"); // 줄 바꿈 추가
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            int max = 2 * n - 1;
+            for (int j = 0; j < max / 2 - i; j++) {
+                output.append(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                output.append("*");
+            }
+            output.append("\n"); // 줄 바꿈 추가
+        }
+
+        System.out.println(output.toString()); // StringBuilder 내용 출력
+    }
 }
