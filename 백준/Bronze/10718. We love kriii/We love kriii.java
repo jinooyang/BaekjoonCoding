e410ca90274ecab7ee5d58e8d054ea2b8f95d941
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-08-27 11:43:05 +0900

    [Bronze V] Title: We love kriii, Time: 80 ms, Memory: 11144 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		String s = "강한친구 대한육군\n" + "강한친구 대한육군";
+		System.out.println(s);
+	}
+}
