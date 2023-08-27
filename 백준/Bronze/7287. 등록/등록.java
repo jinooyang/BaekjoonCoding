a2698964f2ce6e0d5dde59c73e48cf4a60a7011f
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-08-27 11:48:33 +0900

    [Bronze V] Title: 등록, Time: 72 ms, Memory: 11364 KB -BaekjoonHub

--- 백준/Bronze/7287. 등록/등록.java
+++ 백준/Bronze/7287. 등록/등록.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		System.out.println(350);
+		System.out.println(351);
 		System.out.println("tigris3000");
 	}
 }
