63470fb3b2d7b13b2db5040af7a3086bec036617
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-08-30 09:38:43 +0900

    [Silver III] Title: Four Squares, Time: 152 ms, Memory: 11592 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -6,7 +6,6 @@ import java.util.StringTokenizer;
 public class Main {
 	static int n;
 	static Integer ary[];
-	static boolean answerFound = false;
 	static int answer = 5;
 
 	public static void main(String[] args) throws IOException {
@@ -14,36 +13,22 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		ary = new Integer[225];
-		for (int i = 224; i >= 0; i--) {
+		for (int i = 224; i >= 0; i--)
 			ary[i] = i * i;
-		}
-		int history[] = new int[4];
-		dfs(0, 0, 224, history);
+		dfs(0, 0, 224);
 		System.out.println(answer);
 	}
 
-	private static void dfs(int cnt, int sum, int idx, int[] history) {
-		if (sum == n) {
-			// System.out.println("cnt : " + history[0] + " " + history[1] + " " +
-			// history[2] + " " + history[3]);
-			// answerFound = true;
+	private static void dfs(int cnt, int sum, int idx) {
+		if (sum == n)
 			if (cnt < answer)
 				answer = cnt;
-		//	answer = cnt;
-		}
-		if (cnt == 4) {
-			return;
-		}
-		if (answerFound)
+		if (cnt == 4)
 			return;
 		if (sum > n)
 			return;
-		for (int i = idx; i >= 1; i--) {
-			history[cnt] = i;
-			dfs(cnt + 1, sum + ary[i], i, history);
-			history[cnt] = 0;
-			if (answerFound)
-				return;
-		}
+		for (int i = idx; i >= 1; i--)
+			dfs(cnt + 1, sum + ary[i], i);
+
 	}
 }
