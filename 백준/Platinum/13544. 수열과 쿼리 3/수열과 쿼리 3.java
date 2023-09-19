0884dffca478836e0fdada3c1cdf8c88af7e2077
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-09-19 21:32:57 +0900

    [Platinum III] Title: 수열과 쿼리 3, Time: 2260 ms, Memory: 154088 KB -BaekjoonHub

--- 백준/Platinum/13544. 수열과 쿼리 3/수열과 쿼리 3.java
+++ 백준/Platinum/13544. 수열과 쿼리 3/수열과 쿼리 3.java
@@ -8,25 +8,28 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static List<Integer> seg[];
+	static int segCnt;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		seg = new List[4 * n];
-
-		for (int i = 0; i < 4 * n; i++) {
-			seg[i] = new ArrayList<>();
-		}
+		init(1, n, 1);
+//		for (int i = 0; i < 4 * n; i++) {
+//			seg[i] = new ArrayList<>();
+//		}
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= n; i++) {
 			int x = Integer.parseInt(st.nextToken());
 			update(1, n, 1, i, x);
 		}
 
-		for (int i = 0; i < 4 * n; i++) {
-			Collections.sort(seg[i]);
-		}
+		sortSeg(1, n, 1);
+
+//		for (int i = 0; i < 4 * n; i++) {
+//			Collections.sort(seg[i]);
+//		}
 		StringBuilder sb = new StringBuilder();
 		st = new StringTokenizer(br.readLine());
 		int m = Integer.parseInt(st.nextToken());
@@ -41,7 +44,7 @@ public class Main {
 			sb.append(x).append("\n");
 		}
 		System.out.println(sb);
-
+		br.close();
 	}
 
 	public static int query(int s, int e, int idx, int l, int r, int k) {
@@ -68,6 +71,29 @@ public class Main {
 
 		update(s, mid, idx * 2, c, val);
 		update(mid + 1, e, idx * 2 + 1, c, val);
+	}
+
+	public static void init(int s, int e, int idx) {
+		if (s == e) {
+			seg[idx] = new ArrayList<>();
+			return;
+		}
+
+		seg[idx] = new ArrayList<>();
+		int mid = (s + e) >> 1;
+		init(s, mid, idx * 2);
+		init(mid + 1, e, idx * 2 + 1);
+	}
+
+	private static void sortSeg(int s, int e, int idx) {
+		Collections.sort(seg[idx]);
+		if (s == e)
+			return;
+		int mid = (s + e) >> 1;
+
+		sortSeg(s, mid, idx * 2);
+		sortSeg(mid + 1, e, idx * 2 + 1);
+
 	}
 
 	public static int upperBound(List<Integer> list, int k) {
