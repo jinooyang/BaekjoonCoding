4655842d0869f8e7b6bb4e6f39b9b379d479de7e
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-10-08 16:05:33 +0900

    [Silver I] Title: IOIOI, Time: 244 ms, Memory: 31064 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int pn = Integer.parseInt(br.readLine());
+		int k = Integer.parseInt(br.readLine());
+		String text = br.readLine();
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("I");
+		for (int i = 0; i < pn; i++) {
+			sb.append("OI");
+		}
+
+		String pattern = sb.toString();
+		int n = pattern.length();
+		int table[] = new int[n];
+
+		int idx = 0;
+
+		for (int i = 1; i < n; i++) {
+			while (idx > 0 && pattern.charAt(i) != pattern.charAt(idx)) {
+				idx = table[idx - 1];
+			}
+			if (pattern.charAt(i) == pattern.charAt(idx)) {
+				idx++;
+				table[i] = idx;
+			}
+		}
+
+		int m = text.length();
+		int begin = 0;
+		int match = 0;
+		int answer = 0;
+//		StringBuilder sb = new StringBuilder();
+		while (begin <= m - n) {
+			if (match < n && text.charAt(begin + match) == pattern.charAt(match)) {
+				match++;
+				if (match == n) {
+					answer++;
+//					sb.append(begin + 1).append("\n");
+				}
+			} else {
+				if (match == 0)
+					begin++;
+				else {
+					begin += match - table[match - 1];
+					match = table[match - 1];
+				}
+			}
+
+		}
+		System.out.println(answer);
+//		System.out.print(sb);
+
+	}
+}
