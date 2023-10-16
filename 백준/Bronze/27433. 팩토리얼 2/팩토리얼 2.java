c194e7e9020603c6e598c0255d2d169ffaa24bff
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-10-16 10:58:38 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		long answer = 1;
+		while (n > 0) {
+			answer *= n--;
+		}
+		System.out.println(answer);
+	}
+}
