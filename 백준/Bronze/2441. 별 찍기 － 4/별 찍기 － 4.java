ea47b4f5a6a397d163aa88d4d62c16e739d3f927
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-10-16 11:35:01 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 88 ms, Memory: 11624 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i; j++)
+				sb.append(" ");
+
+			for (int j = i; j < n; j++)
+				sb.append("*");
+
+			if (i != n - 1)
+				sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
