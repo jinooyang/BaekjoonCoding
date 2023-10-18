dbd4f047d4648a6e60b8822faa99bdabfecb07f8
jinooyang <102005444+jinooyang@users.noreply.github.com>
2023-10-18 19:25:53 +0900

    [Bronze IV] Title: 과자, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+//		System.setIn(new FileInputStream("test.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int k = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int ans = k * n - m;
+		ans = Math.max(ans, 0);
+		System.out.println(ans);
+	}
+}
