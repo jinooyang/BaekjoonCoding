00f98691f845cfbcd5487adac24a1860d8ad8959
jinooyang <102005444+jinooyang@users.noreply.github.com>
2024-07-21 02:31:22 +0900

    [Bronze II] Title: 이진수 덧셈, Time: 72 ms, Memory: 11684 KB -BaekjoonHub

--- 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.java
+++ 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.java
@@ -4,19 +4,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-       // System.setIn(new FileInputStream("test.txt"));
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         BigInteger binaryNumber1 = new BigInteger(st.nextToken(), 2);
         BigInteger binaryNumber2 = new BigInteger(st.nextToken(), 2);
-
-        BigInteger sum = binaryNumber1.add(binaryNumber2);
-
-        String binarySum = sum.toString(2);
-
-        System.out.println(binarySum);
-
+        System.out.println(binaryNumber1.add(binaryNumber2).toString(2));
     }
-
 }
